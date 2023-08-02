d3330826bfb023bb61a59789d8c0764fb3debb05
flex-akin <akintolafelix2121@gmail.com>
2023-08-02 03:06:37 +0100

    Builder Design Pattern Files

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
-import com.phone.Android;
 import com.phone.Os;
+import com.phone.OsFactory;
+import com.phone.Phone;
+import com.phone.PhoneBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        Os obj = new Android();
-        obj.spec();
+//        OsFactory obj = new OsFactory();
+//        Os phoneOne = obj.FactoryInstance("Android");
+//        Os phoneTwo = obj.FactoryInstance("IOS");
+//
+//        phoneOne.spec();
+//        phoneTwo.spec();
+
+
+        // Builder
+        PhoneBuilder redmi = new PhoneBuilder().setBattery(30);
+        Phone felixPhone = redmi.getPhone();
+        System.out.println(felixPhone);
     }
 }
